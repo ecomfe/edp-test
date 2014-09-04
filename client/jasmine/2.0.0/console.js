6dbdce61184d8fd8d514d7b2d5e966e52773b677
chris <wfsr@foxmail.com>
2014-09-04 22:28:10 +0800

    add jasmine2 stuffs

---
+++ client/jasmine/2.0.0/console.js
@@ -0,0 +1,101 @@
+;(function(jasmine) {
+    var formatFailedStep = function(step) {
+
+        var stack = step.stack;
+        var message = step.message;
+        if(stack) {
+            // remove the trailing dot
+            var firstLine = stack.substring(0, stack.indexOf('\n') - 1);
+            if(message && message.indexOf(firstLine) === -1) {
+                stack = message + '\n' + stack;
+            }
+
+            // remove jasmine stack entries
+            return stack.replace(/\n.+jasmine\.js\w*\:.+(?=(\n|$))/g, '');
+        }
+
+        return message;
+    };
+
+
+
+    /**
+     * 用于 edp-test 的 Reporter 简单实现
+     *
+     * @constructor
+     * @param {Socket} socket socket.io 的实例
+     */
+
+    function Reporter(socket, host) {
+        this.socket = socket;
+        this.host = host;
+
+        this.topResults = new jasmine.ResultsNode({}, '', null);
+        this.currentParent = this.topResults;
+    }
+
+    Reporter.prototype = {
+
+        constructor: Reporter,
+
+        jasmineStarted: function(runner) {
+            this.total = runner.totalSpecsDefined | 0;
+            this.socket.emit('start', this.total);
+        },
+
+        jasmineDone: function(runner) {
+            this.socket.emit('complete', {
+                coverage: window.__coverage__
+            });
+            this.host.done();
+        },
+
+
+        suiteStarted: function (result) {
+            this.currentParent.addChild(result, 'suite');
+            this.currentParent = this.currentParent.last();
+        },
+
+        suiteDone: function (result) {
+            if (this.currentParent !== this.topResults) {
+                this.currentParent = this.currentParent.parent;
+            }
+        },
+
+        specStarted: function (result) {
+            this.currentParent.addChild(result, 'spec');
+        },
+
+        specDone: function(spec) {
+
+            var result = {
+                id: parseInt(spec.id.slice(4), 10),
+                description: spec.description,
+                suite: [],
+                success: spec.status === 'passed',
+                skipped: spec.status === 'pending',
+                log: [],
+                total: this.total
+            };
+
+            var topResults = this.topResults;
+            var currentParent = this.currentParent;
+            while(currentParent && currentParent !== topResults) {
+                result.suite.unshift(currentParent.result.description);
+                currentParent = currentParent.parent;
+            }
+
+            if(spec.status === 'failed') {
+                var steps = spec.failedExpectations;
+                for(var i = 0; i < steps.length; i++) {
+                    result.log.push(formatFailedStep(steps[i]));
+                }
+            }
+
+            this.socket.emit('result', result);
+        }
+    };
+
+    jasmine.EdpReporter = Reporter;
+
+})(window.jasmine);
